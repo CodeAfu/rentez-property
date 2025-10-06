fbc300c2a38fb4f6ef94b076f0d75696a65bae92
Victor24 <166001239+VictorCodexKing@users.noreply.github.com>
2025-10-06 17:52:40 +0800

    HTLM changeee (#1)
    * HTLM changeee
    
    * Update page.tsx
    
    ---------
    
    Co-authored-by: Ibrahim Afzal Shareef <127954747+CodeAfu@users.noreply.github.com>

--- src/app/property/listing/page.tsx
+++ src/app/property/listing/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function PropertyListing() {
-  return <div className="min-h-screen">PropertyListing</div>;
+  return <div className="min-h-screen">PropertyListing2222</div>;
 }
