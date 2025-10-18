eb72e53b73a56af54630380bf6d261c2686729cf
Ibrahim Afzal Shareef <127954747+CodeAfu@users.noreply.github.com>
2025-10-18 15:56:39 +0800

    Created sandbox page, added accordion to organize components, removed color theme page (#6)

---
+++ src/app/test-components/_components/document.submit.tsx
@@ -0,0 +1,6 @@
+import { Input } from "@/components/ui/input";
+import React from "react";
+
+export default function DocumentSubmit() {
+  return <Input type="file" />;
+}
--- src/app/test-components/page.tsx
+++ src/app/test-components/page.tsx
@@ -1,12 +1,23 @@
 import { notFound } from "next/navigation";
 import React from "react";
-import DocusealComponent from "./_components/docuseal-component";
+import DocumentView from "./_components/document-view";
+import DocumentSubmit from "./_components/document.submit";
+import ComponentDisplay from "./_components/component-display";
+import ColorShowcase from "@/components/color-showcase";
 
 export default function TestPage() {
   if (process.env.NODE_ENV !== "development") return notFound();
   return (
-    <div className="min-h-screen sm:pt-8 pt-2">
-      <DocusealComponent />
+    <div className="container mx-auto min-h-screen sm:pt-8 pt-2">
+      <ComponentDisplay title="shadcn Theme Colors">
+        <ColorShowcase />
+      </ComponentDisplay>
+      <ComponentDisplay title="DocuSeal Document View">
+        <DocumentView />
+      </ComponentDisplay>
+      <ComponentDisplay title="DocuSeal Document Submit">
+        <DocumentSubmit />
+      </ComponentDisplay>
     </div>
   );
 }
