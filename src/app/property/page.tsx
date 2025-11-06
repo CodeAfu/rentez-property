031a6eae065f86d14f506133682b321bd86b6e0e
VictorCodexKing <tankiansheng121@gmail.com>
2025-11-06 13:38:52 +0800

    fix deployment issues

--- src/app/property/page.tsx
+++ src/app/property/page.tsx
@@ -1,16 +1,13 @@
 "use client";
+
 import dynamic from 'next/dynamic';
-import { useSearchParams } from 'next/navigation';
 
 const SearchClient = dynamic(() => import('./_components/search-client'), { ssr: false });
 
 export default function TenantsPage() {
-	const searchParams = useSearchParams();
-	console.log('Current pathname:', searchParams.get('search'));
-	
 	return (
 		<section className="min-h-screen p-2 max-w-7xl w-full m-auto">
-				<SearchClient />
+			<SearchClient />
 		</section>
 	);
 }
