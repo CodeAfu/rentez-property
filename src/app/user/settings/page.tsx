cb44387caa910a797ed22adcd4429a9c515de4dd
CodeAfu <ibrahim.afzal1999@gmail.com>
2025-11-19 18:06:36 +0800

    Add suspense boundary

--- src/app/user/settings/page.tsx
+++ src/app/user/settings/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import SettingsLayout from "./_components/settings-layout";
 
 export default function SettingsPage() {
   return (
     <div className="w-full px-2 mx-auto max-w-7xl h-[calc(100dvh-4rem)]">
-      <SettingsLayout />
+      <Suspense>
+        <SettingsLayout />
+      </Suspense>
     </div>
   );
 }
