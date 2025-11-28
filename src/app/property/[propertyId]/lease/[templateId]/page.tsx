fc224b9afe26b0eb27ada8f6aed452b588e1ad11
CodeAfu <ibrahim.afzal1999@gmail.com>
2025-11-28 19:17:38 +0800

    Fixed missing property bug

--- src/app/property/[propertyId]/lease/[templateId]/page.tsx
+++ src/app/property/[propertyId]/lease/[templateId]/page.tsx
@@ -1,14 +1,14 @@
 import DocumentBuilder from "../_components/document-builder";
 
 interface LeasePageProps {
-  params: Promise<{ templateId: string }>;
+  params: Promise<{ propertyId: string; templateId: string }>;
 }
 
 export default async function LeasePage({ params }: LeasePageProps) {
-  const { templateId } = await params;
+  const { propertyId, templateId } = await params;
   return (
     <div className="container mx-auto my-4 min-h-[calc(100dvh-4rem)]">
-      <DocumentBuilder />
+      <DocumentBuilder propertyId={propertyId} />
       <div>{templateId}</div>
     </div>
   );
